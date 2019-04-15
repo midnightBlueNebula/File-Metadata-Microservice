2223ddacb48c0c2cecdbb9d3b4864e21792eb81e
Glitch (fcc-file-metadata-microservic) <none>
2019-04-15 21:42:17 +0000

    😺🍢 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+var path = require('path');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+var upload = multer({dest:'uploads/'})
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse',upload.single('upfile') ,function(req,res){
+  var name = req.file.originalname;
+  var type = req.file.mimetype
+  var size = req.file.size;
+  res.send({'name':name,'type':type,'size':size});
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
